387fb0023a02fe1ba95646b106fbeeae6e815263
Quoc Thang <comehere.thang@gmail.com>
2024-05-10 22:11:53 +0700

    #22.5 Validation
    #22.5 Validation

--- Backend/src/users/dto/create-user.dto.ts
+++ Backend/src/users/dto/create-user.dto.ts
@@ -1,5 +1,21 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
+//data transfer object for creating a user
 export class CreateUserDto {
+  @IsEmail(
+    {},
+    {
+      message: 'Email không đúng định dạng',
+    },
+  )
+  @IsNotEmpty({
+    message: 'Email không được để trống',
+  })
   email: string;
+
+  @IsNotEmpty({
+    message: 'Password không được để trống',
+  })
   password: string;
   name: string;
   address: string;
