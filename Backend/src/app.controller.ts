7a7899773fafb5dfa9c5b14ccb247df6da26687d
Quoc Thang <comehere.thang@gmail.com>
2024-05-11 14:23:05 +0700

    #39. Sử dụng JWT
    #39. Sử dụng JWT

--- Backend/src/app.controller.ts
+++ Backend/src/app.controller.ts
@@ -3,17 +3,19 @@ import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+import { AuthService } from 'src/auth/auth.service';
 
 @Controller()
 export class AppController {
   constructor(
     private readonly appService: AppService,
     private configService: ConfigService,
+    private authService: AuthService,
   ) {}
 
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
--- Backend/src/app.module.ts
+++ Backend/src/app.module.ts
@@ -25,7 +25,6 @@ import { AuthModule } from './auth/auth.module';
     }),
 
     UsersModule,
-
     AuthModule,
   ],
   controllers: [AppController],
